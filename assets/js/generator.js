ab24283a68ef2d70ce82d9c80476f7d5bfffa34d
Wilfred Gonzalez <wilfredg92@gmail.com>
2021-09-12 22:45:23 -0400

    Update w/ generator...and some bugs

--- assets/js/generator.js
+++ assets/js/generator.js
@@ -2,7 +2,7 @@
 
 var generatePassword = function() {
   var confirmLength = prompt("How many characters would you like your password to have? (8-128)");
-  if (confirmLength > 8 && confirmLength < 128) {
+  if (confirmLength > 7 && confirmLength < 129) {
       alert("Your password will have " + confirmLength + " characters in it.");
   } else {
       alert("Entry not valid. Please enter a valid length (8-128 characters)");
@@ -11,43 +11,46 @@ var generatePassword = function() {
   var confirmLowercase = confirm("Will your password contain lowercase letters? Select 'OK' for yes or 'Cancel' for no.");
       if (confirmLowercase === true) {
           alert("Your password will have lowercase letters in it.");
+          var lowercase = "abcdefghijklmonpqrstuvwxyz";
       } else {
           alert("Your password will NOT have lowercase letters in it.");
+          var lowercase = null;
       }
   var confirmUppercase = confirm("Will your password contain uppercase letters? Select 'OK' for yes or 'Cancel' for no.");
       if (confirmUppercase === true) {
           alert("Your password will have uppercase letters in it.");
+          var uppercase = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
       } else {
           alert("Your password will NOT have uppercase letters in it.");
+          var uppercase = null;
       }
   var confirmNumbers = confirm("Will your password contain numbers? Select 'OK' for yes or 'Cancel' for no.");
       if (confirmNumbers === true) {
           alert("Your password will have numbers in it.");
+          var numbers = "0123456789"
       } else {
           alert("Your password will NOT have numbers in it.");
+          var numbers = null;
       }
   var confirmSpecialcharacters = confirm("Will your password contain special characters? Select 'OK' for yes or 'Cancel' for no.");
       if (confirmSpecialcharacters === true) {
           alert("Your password will have special characters in it.");
+          var specialCharacters = "~!@#$%^&*()-+=|<>?/"
       } else {
           alert("Your password will NOT have special characters in it.");
-      }
-      if (confirm.confirmLowercase === false && confirm.confirmUppercase === false && confirm.confirmNUmbers === false && confirm.confirmSpecialCharacters === false) {
-          alert("🤦 Lets do this again 🤦");
-          return generatePassword(confirmLength);
+          var specialCharacters = null;
+        }
+      if (confirmLowercase === false && confirmUppercase === false && confirmNumbers === false && confirmSpecialCharacters === false) {
+          return generatePassword();
       } else {
-          alert("almost there");
-      }
+          var passwordString = (lowercase + uppercase + numbers + specialCharacters);
 
-  }
-  
-
-
-
-
-
-
-
+          for (var i = 0; i < confirmLength; i++) {
+            password = passwordString.charAt(Math.floor(Math.random() * passwordString.confirmLength));
+          };
+        }
+  return password;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
